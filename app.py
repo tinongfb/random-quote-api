c21eed4ae72e2cd66ef99a9c19c383f7f360baa1
Christine Ong <christine.ong@sviisca.com>
2025-04-14 15:40:00 +0800

    Initial commit: Random Quote API

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+import json
+import random
+
+app = Flask(__name__)
+
+# Load quotes from JSON file
+with open('quotes.json') as f:
+    quotes = json.load(f)
+
+@app.route('/quote', methods=['GET'])
+def get_quote():
+    return jsonify(random.choice(quotes))
+
+if __name__ == '__main__':
+    app.run(debug=True)
